af4859fd9b0ad4643fa8c52a1ca5ed2554b19962
Will Weaver <monkey@buildingbananas.com>
2023-07-04 22:58:14 -0400

    Updating s3 bucket policy approach

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -16,50 +16,29 @@ resource "aws_s3_bucket_website_configuration" "public_bucket_website_config" {
 
 }
 
-resource "aws_s3_bucket_ownership_controls" "public_bucket" {
-  bucket = aws_s3_bucket.public_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "public_bucket" {
-  bucket = aws_s3_bucket.public_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "public_bucket" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.public_bucket,
-    aws_s3_bucket_public_access_block.public_bucket,
-  ]
-
+resource "aws_s3_bucket_acl" "public_bucket_acl" {
   bucket = aws_s3_bucket.public_bucket.id
   acl    = "public-read"
 }
 
-# resource "aws_s3_bucket_policy" "public_bucket_policy" {
-#   bucket = aws_s3_bucket.public_bucket.id
-#   policy = <<POLICY
-# {
-#   "Version": "2012-10-17",
-#   "Id": "PublicReadAllow",
-#   "Statement": [
-#     {
-#       "Sid": "PublicReadGetObject",
-#       "Effect": "Allow",
-#       "Principal": "*",
-#       "Action": "s3:GetObject",
-#       "Resource": "arn:aws:s3:::${var.environment}${var.domain_name}/*"
-#     }
-#   ]
-# }
-# POLICY
-# }
+resource "aws_s3_bucket_policy" "public_bucket_policy" {
+  bucket = aws_s3_bucket.public_bucket.id
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "PublicReadAllow",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${var.environment}${var.domain_name}/*"
+    }
+  ]
+}
+POLICY
+}
 
 resource "aws_s3_object" "index" {
   bucket       = aws_s3_bucket.public_bucket.bucket
@@ -67,4 +46,3 @@ resource "aws_s3_object" "index" {
   content      = templatefile("./public_webpage/index.tftpl", { lambda_url = "${aws_lambda_function_url.lambda_url.function_url}" })
   content_type = "text/html"
 }
-
